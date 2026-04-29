070ac3688227c05c44d54548a79cab6411762ba5
savindi silva <desilvasavindi0812@gmail.com>
2026-04-29 11:00:30 +0530

    You need this to search

--- lib/screens/map_picker_screen.dart
+++ lib/screens/map_picker_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http; // Search කරන්න මේක ඕනේ
+import 'package:http/http.dart' as http; // You need this to search
 import 'dart:convert';
 
 class MapPickerScreen extends StatefulWidget {
